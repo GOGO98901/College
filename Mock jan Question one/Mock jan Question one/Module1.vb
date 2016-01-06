b49b9f491414d485772612de87ea10b6fb86ea98
Rory Claasen (At College) <roryclaasen@live.co.uk>
2016-01-06 15:54:11 +0000

    mock test JAN

---
+++ Mock jan Question one/Mock jan Question one/Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim playerOneScore As Integer = 0
+        Dim playerTwoScore As Integer = 0
+        Console.WriteLine("How many games were played? (as integer)")
+        Dim numberOfGames As Integer = Console.ReadLine()
+        For noGame As Integer = 1 To numberOfGames
+            Console.WriteLine("Round " & noGame & ": Did player one win this round? (y/n)")
+            Dim input As Char = Console.ReadLine()
+            If input = "y" Or input = "Y" Then
+                playerOneScore += 1
+            Else
+                playerTwoScore += 1
+            End If
+        Next
+        Console.WriteLine()
+        Console.WriteLine("Player one scored " & playerOneScore)
+        Console.WriteLine("Player Two scored " & playerTwoScore)
+        Console.ReadLine()
+    End Sub
+
+End Module
