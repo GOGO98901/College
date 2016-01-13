7af37fcfb971ffc4c0fc37a74c29eaa2a21315b4
Rory Claasen (At College) <roryclaasen@live.co.uk>
2016-01-13 15:24:56 +0000

    Updated program

--- writingByteTest/writingByteTest/Module1.vb
+++ writingByteTest/writingByteTest/Module1.vb
@@ -1,7 +1,19 @@
 ﻿Module Module1
+    Structure test
+        Dim testNum As Single
+        Dim testChr As Char
+        Dim testSrt As String
+    End Structure
 
     Sub Main()
+        Dim testData As test
+        testData.testNum = 1.25
+        testData.testChr = "P"
+        testData.testSrt = "Yeah! Computing"
 
+        FileOpen(1, "BinaryTest.bin", OpenMode.Binary, OpenAccess.Write)
+        FilePut(1, testData)
+        FileClose(1)
     End Sub
 
 End Module
