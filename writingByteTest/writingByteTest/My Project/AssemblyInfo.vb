b83e02e3b81cbd885093f783278587bbcfd2d14a
Rory Claasen (At College) <roryclaasen@live.co.uk>
2016-01-13 15:10:23 +0000

    started on byte writing

---
+++ writingByteTest/writingByteTest/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("writingByteTest")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Godalming College")> 
+<Assembly: AssemblyProduct("writingByteTest")> 
+<Assembly: AssemblyCopyright("Copyright © Godalming College 2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("7e6e7a74-32b3-4234-900f-e36a682fdab1")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
