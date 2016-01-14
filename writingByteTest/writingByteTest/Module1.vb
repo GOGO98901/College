60dad30ca21aa18f2d120c324861c6dbbd9fdff6
Rory Claasen (At College) <roryclaasen@live.co.uk>
2016-01-14 12:24:31 +0000

    working on an open method

--- writingByteTest/writingByteTest/Module1.vb
+++ writingByteTest/writingByteTest/Module1.vb
@@ -1,19 +1,48 @@
-﻿Module Module1
+﻿Imports System.IO
+Module Module1
+
     Structure test
-        Dim testNum As Single
-        Dim testChr As Char
         Dim testSrt As String
+        Dim testNum As Integer
+        Dim testChr As Char
     End Structure
 
     Sub Main()
-        Dim testData As test
-        testData.testNum = 1.25
-        testData.testChr = "P"
+        Dim fileName As String = "BinaryTest.bin"
+
+        Dim recovered As New test
+        Dim testData As New test
+        testData.testNum = 65
+        testData.testChr = "*"
         testData.testSrt = "Yeah! Computing"
 
-        FileOpen(1, "BinaryTest.bin", OpenMode.Binary, OpenAccess.Write)
+
+        FileOpen(1, fileName, OpenMode.Binary, OpenAccess.Write)
         FilePut(1, testData)
         FileClose(1)
+
+        Dim objBR As BinaryReader
+        Dim objFS As FileStream
+
+        objFS = New FileStream(fileName, FileMode.Open, FileAccess.Read)
+        objBR = New BinaryReader(objFS)
+        objBR.BaseStream.Seek(0, SeekOrigin.Begin)
+
+        While objFS.Position < objFS.Length
+            recovered = Nothing
+            With recovered
+                .testSrt = objBR.ReadString
+                .testNum = objBR.ReadInt32
+                .testChr = objBR.ReadChar
+            End With
+        End While
+        objBR.Close()
+        objFS.Close()
+
+        Console.WriteLine(recovered)
+
+
+        Console.ReadLine()
     End Sub
 
 End Module
