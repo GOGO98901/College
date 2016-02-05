b1422a559443f9e2fa2f0f051e9d3e9eebb1896d
Rory Claasen (At College) <roryclaasen@live.co.uk>
2016-02-05 10:03:16 +0000

    working on dectyptor

--- oneTimePad/oneTimePad/Module1.vb
+++ oneTimePad/oneTimePad/Module1.vb
@@ -1,15 +1,19 @@
-﻿Module Module1
+﻿Imports System.IO
+Module Module1
     Dim Plaintext, Ciphertext, OneTimePad As String
-    Dim choice As Integer
+    Dim choice As String
 
     Sub Main()
         Do
             DisplayMenu()
             choice = Console.ReadLine
-            If choice = 1 Then EncryptMessage()
-            If choice = 2 Then DecryptMessage()
-        Loop Until choice = 3
+            If IsNumeric(choice) Then
+                If choice = 1 Then EncryptMessage()
+                If choice = 2 Then DecryptMessage()
+            End If
+        Loop Until choice = "3"
     End Sub
+
     Sub DisplayMenu()
         Console.Clear()
         Console.WriteLine("1. Encrypt New Message.")
@@ -17,6 +21,7 @@
         Console.WriteLine("3. Quit.")
         Console.Write("Enter Choice: ")
     End Sub
+
     Sub EncryptMessage()
         Console.Write("Enter New Message: ")
         Plaintext = Console.ReadLine.ToUpper
@@ -27,7 +32,7 @@
         Loop Until Len(OneTimePad) >= Len(Plaintext)
         Ciphertext = ""
         For counter = 0 To Len(Plaintext) - 1
-            Ciphertext = Ciphertext + Chr((((asc(Plaintext(counter)) - 64) + (asc(OneTimePad(counter)) - 64)) Mod 26) + 64)
+            Ciphertext = Ciphertext + Chr((((Asc(Plaintext(counter)) - 64) + (Asc(OneTimePad(counter)) - 64)) Mod 26) + 64)
         Next
         Console.Write(Ciphertext)
         FileOpen(1, "Message.txt", OpenMode.Output)
@@ -36,7 +41,38 @@
         Print(2, OneTimePad)
         FileClose(1, 2)
     End Sub
-    Sub DecryptMessage()
 
+    Sub DecryptMessage()
+        If System.IO.File.Exists("Message.txt") Then
+            If System.IO.File.Exists("Key.txt") Then
+                Console.Clear()
+
+                Dim messageE, messageD, key As String
+                Using reader As StreamReader = New StreamReader("Message.txt")
+                    messageE = reader.ReadLine
+                End Using
+                Using reader As StreamReader = New StreamReader("Key.txt")
+                    key = reader.ReadLine
+                End Using
+                Console.WriteLine("Message: " & messageE)
+                Console.WriteLine("    Key: " & key)
+                'Console.WriteLine(vbCrLf & "Decrypting")
+
+                messageD = ""
+                For counter = 0 To Len(messageE) - 1
+                    Dim encodedChar As Integer = Asc(messageE(counter)) - 64
+                    Dim KeyChar As Integer = Asc(key(counter)) - 64
+
+                    messageD += Chr((encodedChar - KeyChar) + 64)
+                Next
+
+                Console.WriteLine(vbCrLf & " Output: " & messageD)
+            Else
+                Console.WriteLine("No key found to decrypt message")
+            End If
+        Else
+            Console.WriteLine("No message found to decrypt")
+        End If
+        Console.ReadLine()
     End Sub
 End Module
