41519a3feef49917a26182f3aad7be392cc2b30f
Rory Claasen (At College) <roryclaasen@live.co.uk>
2016-06-15 15:41:51 +0100

    added monster code

---
+++ MonsterStructured/MonsterStructured/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("MonsterStructured")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Godalming College")> 
+<Assembly: AssemblyProduct("MonsterStructured")> 
+<Assembly: AssemblyCopyright("Copyright © Godalming College 2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("355e7d15-f720-4c1b-b578-235c7340ee77")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
