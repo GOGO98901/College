d3d7ff3e8455f4db1e8f20b9fc36cad4ce335b70
Rory Claasen (At College) <roryclaasen@live.co.uk>
2016-06-24 10:11:20 +0100

    ms access test project

---
+++ AccessDBtest/AccessDBtest/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnDBCreate = New System.Windows.Forms.Button()
+        Me.btnDBOpen = New System.Windows.Forms.Button()
+        Me.btnDBremove = New System.Windows.Forms.Button()
+        Me.btnDBclose = New System.Windows.Forms.Button()
+        Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
+        Me.LineShape1 = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.btnDBstudentTable = New System.Windows.Forms.Button()
+        Me.btnDBexamTable = New System.Windows.Forms.Button()
+        Me.btnDBresultTable = New System.Windows.Forms.Button()
+        Me.btnDBaddFields = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnDBCreate
+        '
+        Me.btnDBCreate.Location = New System.Drawing.Point(12, 12)
+        Me.btnDBCreate.Name = "btnDBCreate"
+        Me.btnDBCreate.Size = New System.Drawing.Size(109, 23)
+        Me.btnDBCreate.TabIndex = 0
+        Me.btnDBCreate.Text = "Create DataBase"
+        Me.btnDBCreate.UseVisualStyleBackColor = True
+        '
+        'btnDBOpen
+        '
+        Me.btnDBOpen.Location = New System.Drawing.Point(12, 70)
+        Me.btnDBOpen.Name = "btnDBOpen"
+        Me.btnDBOpen.Size = New System.Drawing.Size(109, 23)
+        Me.btnDBOpen.TabIndex = 1
+        Me.btnDBOpen.Text = "Open DataBase"
+        Me.btnDBOpen.UseVisualStyleBackColor = True
+        '
+        'btnDBremove
+        '
+        Me.btnDBremove.Location = New System.Drawing.Point(12, 41)
+        Me.btnDBremove.Name = "btnDBremove"
+        Me.btnDBremove.Size = New System.Drawing.Size(109, 23)
+        Me.btnDBremove.TabIndex = 2
+        Me.btnDBremove.Text = "Remove DataBase"
+        Me.btnDBremove.UseVisualStyleBackColor = True
+        '
+        'btnDBclose
+        '
+        Me.btnDBclose.Location = New System.Drawing.Point(12, 99)
+        Me.btnDBclose.Name = "btnDBclose"
+        Me.btnDBclose.Size = New System.Drawing.Size(109, 23)
+        Me.btnDBclose.TabIndex = 3
+        Me.btnDBclose.Text = "Close DataBase"
+        Me.btnDBclose.UseVisualStyleBackColor = True
+        '
+        'ShapeContainer1
+        '
+        Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
+        Me.ShapeContainer1.Name = "ShapeContainer1"
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.LineShape1})
+        Me.ShapeContainer1.Size = New System.Drawing.Size(599, 403)
+        Me.ShapeContainer1.TabIndex = 4
+        Me.ShapeContainer1.TabStop = False
+        '
+        'LineShape1
+        '
+        Me.LineShape1.Name = "LineShape1"
+        Me.LineShape1.X1 = 133
+        Me.LineShape1.X2 = 133
+        Me.LineShape1.Y1 = -22
+        Me.LineShape1.Y2 = 428
+        '
+        'btnDBstudentTable
+        '
+        Me.btnDBstudentTable.Location = New System.Drawing.Point(147, 11)
+        Me.btnDBstudentTable.Name = "btnDBstudentTable"
+        Me.btnDBstudentTable.Size = New System.Drawing.Size(106, 23)
+        Me.btnDBstudentTable.TabIndex = 5
+        Me.btnDBstudentTable.Text = "Add Student Table"
+        Me.btnDBstudentTable.UseVisualStyleBackColor = True
+        '
+        'btnDBexamTable
+        '
+        Me.btnDBexamTable.Location = New System.Drawing.Point(147, 41)
+        Me.btnDBexamTable.Name = "btnDBexamTable"
+        Me.btnDBexamTable.Size = New System.Drawing.Size(106, 23)
+        Me.btnDBexamTable.TabIndex = 6
+        Me.btnDBexamTable.Text = "Add Exam Table"
+        Me.btnDBexamTable.UseVisualStyleBackColor = True
+        '
+        'btnDBresultTable
+        '
+        Me.btnDBresultTable.Location = New System.Drawing.Point(147, 70)
+        Me.btnDBresultTable.Name = "btnDBresultTable"
+        Me.btnDBresultTable.Size = New System.Drawing.Size(106, 23)
+        Me.btnDBresultTable.TabIndex = 7
+        Me.btnDBresultTable.Text = "Add Result Table"
+        Me.btnDBresultTable.UseVisualStyleBackColor = True
+        '
+        'btnDBaddFields
+        '
+        Me.btnDBaddFields.Location = New System.Drawing.Point(147, 99)
+        Me.btnDBaddFields.Name = "btnDBaddFields"
+        Me.btnDBaddFields.Size = New System.Drawing.Size(106, 23)
+        Me.btnDBaddFields.TabIndex = 8
+        Me.btnDBaddFields.Text = "Add extra Fields"
+        Me.btnDBaddFields.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(599, 403)
+        Me.Controls.Add(Me.btnDBaddFields)
+        Me.Controls.Add(Me.btnDBresultTable)
+        Me.Controls.Add(Me.btnDBexamTable)
+        Me.Controls.Add(Me.btnDBstudentTable)
+        Me.Controls.Add(Me.btnDBclose)
+        Me.Controls.Add(Me.btnDBremove)
+        Me.Controls.Add(Me.btnDBOpen)
+        Me.Controls.Add(Me.btnDBCreate)
+        Me.Controls.Add(Me.ShapeContainer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnDBCreate As System.Windows.Forms.Button
+    Friend WithEvents btnDBOpen As System.Windows.Forms.Button
+    Friend WithEvents btnDBremove As System.Windows.Forms.Button
+    Friend WithEvents btnDBclose As System.Windows.Forms.Button
+    Friend WithEvents ShapeContainer1 As Microsoft.VisualBasic.PowerPacks.ShapeContainer
+    Friend WithEvents LineShape1 As Microsoft.VisualBasic.PowerPacks.LineShape
+    Friend WithEvents btnDBstudentTable As System.Windows.Forms.Button
+    Friend WithEvents btnDBexamTable As System.Windows.Forms.Button
+    Friend WithEvents btnDBresultTable As System.Windows.Forms.Button
+    Friend WithEvents btnDBaddFields As System.Windows.Forms.Button
+
+End Class
---
+++ AccessDBtest/AccessDBtest/Form1.vb
@@ -0,0 +1,129 @@
+﻿Imports ADOX
+Imports System.Data.OleDb
+Imports System.IO
+
+Public Class Form1
+    Dim data As New DataBase()
+    Private Sub btnDataBaseCreate(sender As System.Object, e As System.EventArgs) Handles btnDBCreate.Click
+        data.CreateDataBase()
+    End Sub
+
+    Private Sub btnDataBaseRemove(sender As System.Object, e As System.EventArgs) Handles btnDBremove.Click
+        data.RemoveDataBase()
+    End Sub
+
+    Private Sub btnDataBaseOpen(sender As System.Object, e As System.EventArgs) Handles btnDBOpen.Click
+        data.OpenDataBase()
+    End Sub
+
+    Private Sub btnDataBaseClose(sender As System.Object, e As System.EventArgs) Handles btnDBclose.Click
+        data.CloseDataBase()
+    End Sub
+
+    Private Sub btnDataBaseAddStudentTable(sender As System.Object, e As System.EventArgs) Handles btnDBstudentTable.Click
+        data.AddTableStudent()
+    End Sub
+
+    Private Sub btnDataBaseAddExamTable(sender As System.Object, e As System.EventArgs) Handles btnDBexamTable.Click
+        data.AddTableExam()
+    End Sub
+
+    Private Sub btnDataBaseAddResultTable(sender As System.Object, e As System.EventArgs) Handles btnDBresultTable.Click
+        data.AddTableResults()
+    End Sub
+
+    Private Sub btnDataBaseAddFields(sender As System.Object, e As System.EventArgs) Handles btnDBaddFields.Click
+        data.CreateField("Exams", "[MAxScore] Int")
+        data.CreateField("Students", "[DateofBirth] Varchar(20)")
+        data.CreateField("Students", "[Gender] Varchar(20)")
+        data.CreateField("Results", "[ExamDate] Int")
+    End Sub
+End Class
+
+Class DataBase
+    Const DataBaseFileName As String = "Test.mdb"
+    Dim mycommand As OleDbCommand
+    Dim myconnection As OleDbConnection
+    Dim myReader As OleDbDataReader
+    Dim SQLstr As String
+
+    Sub CreateDataBase()
+        Dim cat As Catalog = New Catalog()
+        Try
+            cat.Create("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & DataBaseFileName & ";Jet OLEDB:Engine Type=5")
+            MsgBox("Database Created Successfully")
+        Catch ex As Exception
+            MsgBox("Database Creation failed " & ex.Message)
+        End Try
+        cat = Nothing
+    End Sub
+
+    Sub RemoveDataBase()
+        Try
+            System.IO.File.Delete(DataBaseFileName)
+            MsgBox("DataBase Deleted")
+        Catch ex As Exception
+            MsgBox("Database not Deleted " & ex.Message)
+        End Try
+    End Sub
+
+    Sub OpenDataBase()
+        Try
+            myconnection = New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & DataBaseFileName)
+            myconnection.Open()
+            MsgBox("Database Opened Successfully")
+        Catch ex As Exception
+            MsgBox("Database not opened " & ex.Message)
+        End Try
+    End Sub
+
+    Sub CloseDataBase()
+        Try
+            myconnection.Close()
+            MsgBox("Database Closed Successfully")
+        Catch ex As Exception
+            MsgBox("Database not Closed " & ex.Message)
+        End Try
+    End Sub
+
+    Sub AddTableStudent()
+        Try
+            RunNonQuery("CREATE TABLE [Students] ([Username] Varchar(20) CONSTRAINT PrimaryKey PRIMARY KEY, [FirstName] Varchar(25), [Surname] Varchar(25))")
+            MsgBox("Students Table created")
+        Catch ex As Exception
+            MsgBox("Students Table not Created " & ex.Message)
+        End Try
+    End Sub
+
+    Sub AddTableExam()
+        Try
+            RunNonQuery("CREATE TABLE [Exams] ([ExamCode] Varchar(6) CONSTRAINT PrimaryKey PRIMARY KEY, [ExamTitle] Varchar(25), [Subject] Varchar(25))")
+            MsgBox("Exams Table created")
+        Catch ex As Exception
+            MsgBox("Exams Table not Created " & ex.Message)
+        End Try
+    End Sub
+
+    Sub AddTableResults()
+        Try
+            RunNonQuery("CREATE TABLE [Results] ([ResultID] Varchar(6) CONSTRAINT PrimaryKey PRIMARY KEY, [UserName] Varchar(20), [ExamCode] Varchar(6), [Score] Int)")
+            MsgBox("Results Table created")
+        Catch ex As Exception
+            MsgBox("Results Table not Created " & ex.Message)
+        End Try
+    End Sub
+
+    Sub CreateField(Table As String, Command As String)
+        Try
+            RunNonQuery("ALTER TABLE [" & Table & "] ADD COLUMN " & Command)
+            MsgBox("Field Added Sucessfully")
+        Catch ex As Exception
+            MsgBox("Field NOT Added Sucessfully " & ex.Message)
+        End Try
+    End Sub
+
+    Sub RunNonQuery(DDLstr As String)
+        mycommand = New OleDb.OleDbCommand(DDLstr, myconnection)
+        mycommand.ExecuteNonQuery()
+    End Sub
+End Class
