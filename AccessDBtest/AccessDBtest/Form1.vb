ebc659d629b119d7940569415345ffcae56db32e
Rory Claasen (At College) <roryclaasen@live.co.uk>
2016-06-28 13:00:26 +0100

    added new code

--- AccessDBtest/AccessDBtest/Form1.vb
+++ AccessDBtest/AccessDBtest/Form1.vb
@@ -38,6 +38,26 @@ Public Class Form1
         data.CreateField("Students", "[Gender] Varchar(20)")
         data.CreateField("Results", "[ExamDate] Int")
     End Sub
+
+    Private Sub btnAddStudent_Click(sender As System.Object, e As System.EventArgs) Handles btnAddStudent.Click
+        Dim status As Boolean = data.InsertRecord("Students", "[Username], [FirstName], [Surname]", "'" & StudentUsername.Text & "','" & StudentFirstName.Text & "','" & StudentSurname.Text & "'")
+        If status Then
+            StudentUsername.Text = ""
+            StudentFirstName.Text = ""
+            StudentSurname.Text = ""
+        End If
+    End Sub
+
+    Private Sub btnAddExam_Click(sender As System.Object, e As System.EventArgs) Handles btnAddExam.Click
+        Dim status As Boolean = data.InsertRecord("Exams", "[ExamCode], [ExamTitle], [Subject], [Weight], [MaxScore]", "'" & examCode.Text & "','" & examTitle.Text & "','" & examSubject.Text & "','" & examWeight.Text & "','" & examMaxScore.Text & "'")
+        If status Then
+            examCode.Text = ""
+            examTitle.Text = ""
+            examSubject.Text = ""
+            examWeight.Text = ""
+            examMaxScore.Text = ""
+        End If
+    End Sub
 End Class
 
 Class DataBase
@@ -47,80 +67,107 @@ Class DataBase
     Dim myReader As OleDbDataReader
     Dim SQLstr As String
 
-    Sub CreateDataBase()
+    Function CreateDataBase() As Boolean
         Dim cat As Catalog = New Catalog()
         Try
             cat.Create("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & DataBaseFileName & ";Jet OLEDB:Engine Type=5")
+            cat = Nothing
             MsgBox("Database Created Successfully")
+            Return True
         Catch ex As Exception
             MsgBox("Database Creation failed " & ex.Message)
+            Return False
         End Try
-        cat = Nothing
-    End Sub
+    End Function
 
-    Sub RemoveDataBase()
+    Function RemoveDataBase() As Boolean
         Try
             System.IO.File.Delete(DataBaseFileName)
             MsgBox("DataBase Deleted")
+            Return True
         Catch ex As Exception
             MsgBox("Database not Deleted " & ex.Message)
+            Return False
         End Try
-    End Sub
+    End Function
 
-    Sub OpenDataBase()
+    Function OpenDataBase() As Boolean
         Try
             myconnection = New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & DataBaseFileName)
             myconnection.Open()
             MsgBox("Database Opened Successfully")
+            Return True
         Catch ex As Exception
             MsgBox("Database not opened " & ex.Message)
+            Return False
         End Try
-    End Sub
+    End Function
 
-    Sub CloseDataBase()
+    Function CloseDataBase() As Boolean
         Try
             myconnection.Close()
             MsgBox("Database Closed Successfully")
+            Return True
         Catch ex As Exception
             MsgBox("Database not Closed " & ex.Message)
+            Return False
         End Try
-    End Sub
+    End Function
 
-    Sub AddTableStudent()
+    Function AddTableStudent() As Boolean
         Try
             RunNonQuery("CREATE TABLE [Students] ([Username] Varchar(20) CONSTRAINT PrimaryKey PRIMARY KEY, [FirstName] Varchar(25), [Surname] Varchar(25))")
             MsgBox("Students Table created")
+            Return True
         Catch ex As Exception
             MsgBox("Students Table not Created " & ex.Message)
+            Return False
         End Try
-    End Sub
+    End Function
 
-    Sub AddTableExam()
+    Function AddTableExam() As Boolean
         Try
             RunNonQuery("CREATE TABLE [Exams] ([ExamCode] Varchar(6) CONSTRAINT PrimaryKey PRIMARY KEY, [ExamTitle] Varchar(25), [Subject] Varchar(25))")
             MsgBox("Exams Table created")
+            Return True
         Catch ex As Exception
             MsgBox("Exams Table not Created " & ex.Message)
+            Return False
         End Try
-    End Sub
+    End Function
 
-    Sub AddTableResults()
+    Function AddTableResults() As Boolean
         Try
             RunNonQuery("CREATE TABLE [Results] ([ResultID] Varchar(6) CONSTRAINT PrimaryKey PRIMARY KEY, [UserName] Varchar(20), [ExamCode] Varchar(6), [Score] Int)")
             MsgBox("Results Table created")
+            Return True
         Catch ex As Exception
             MsgBox("Results Table not Created " & ex.Message)
+            Return False
         End Try
-    End Sub
+    End Function
 
-    Sub CreateField(Table As String, Command As String)
+    Function CreateField(Table As String, Command As String) As Boolean
         Try
             RunNonQuery("ALTER TABLE [" & Table & "] ADD COLUMN " & Command)
             MsgBox("Field Added Sucessfully '" & Command & "'")
+            Return True
         Catch ex As Exception
             MsgBox("Field NOT Added Sucessfully '" & Command & "' " & ex.Message)
+            Return False
         End Try
-    End Sub
+    End Function
+
+    Function InsertRecord(Table As String, Cells As String, Values As String) As Boolean
+        Try
+            RunNonQuery("INSERT INTO " & Table & "(" & Cells & ") VALUES (" & Values & ")")
+            MsgBox("Record Added Sucessfully")
+            Return True
+        Catch ex As Exception
+            MsgBox("Record NOT Added Sucessfully '" & Command() & "' " & ex.Message)
+            Return False
+        End Try
+    End Function
 
     Sub RunNonQuery(DDLstr As String)
         mycommand = New OleDb.OleDbCommand(DDLstr, myconnection)
