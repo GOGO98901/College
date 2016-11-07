d74e728182c6bf7d811054be31ea9313ef0ddf1b
Rory Claasen (At College) <roryclaasen@live.co.uk>
2016-11-07 14:34:19 +0000

    added new sorting method

--- SortingMethods/SortingMethods/Module1.vb
+++ SortingMethods/SortingMethods/Module1.vb
@@ -12,7 +12,8 @@
                     Console.Write("Enter the size of the array of random numbers (Max " & (Integer.MaxValue / 16) & "): ")
                     N = Console.ReadLine
                     GenerateArray(Numbers, N)
-                    DisplayArray(Numbers, N)
+                    'DisplayArray(Numbers, N)
+                    Console.WriteLine("Created a random array with " & N & " entries")
                     Console.Write("Press ENTER to continue: ")
                     Console.ReadLine()
                 Case 2
@@ -31,7 +32,7 @@
                     Console.ReadLine()
                 Case 4
                     StartTime = My.Computer.Clock.TickCount
-                    InsertionSort(Numbers, N)
+                    MergeSort(Numbers, N, 0, N)
                     StopTime = My.Computer.Clock.TickCount
                     DisplayArray(Numbers, N)
                     Console.Write("Run Time = " & StopTime - StartTime & " mS")
@@ -53,7 +54,7 @@
         Console.WriteLine("1) Generate array of random numbers")
         Console.WriteLine("2) Exchange Sort")
         Console.WriteLine("3) Bubble Sort")
-        Console.WriteLine("4) Insertion Sort")
+        Console.WriteLine("4) Merge Sort")
         Console.WriteLine("5) Partition Sort")
         Console.WriteLine("6) Quit")
         Console.WriteLine()
@@ -104,9 +105,47 @@
             i += 1
         End While
     End Sub
-    Sub InsertionSort(Numbers() As Integer, Limit As Integer)
-        'write Insertion Sort code here
+
+    Sub MergeSort(Numbers() As Integer, Limit As Integer, First As Integer, Last As Integer)
+        Dim Mid As Integer
+        If Last > First Then
+            Mid = (Last + First) \ 2
+            MergeSort(Numbers, Limit, First, Mid)
+            MergeSort(Numbers, Limit, Mid + 1, Last)
+            Merge(Numbers, Limit, First, Mid, Last)
+        End If
     End Sub
+
+    Sub Merge(Numbers() As Integer, Limit As Integer, StartPtr As Integer, MidPtr As Integer, EndPtr As Integer)
+        Dim TempStore(Limit) As Integer
+        Dim LeftCounter, RightCounter, Counter, CopyCounter As Integer
+        For CopyCounter = StartPtr To EndPtr
+            TempStore(CopyCounter) = Numbers(CopyCounter)
+        Next
+        LeftCounter = StartPtr
+        RightCounter = MidPtr + 1
+        Counter = StartPtr
+        While LeftCounter <= MidPtr And RightCounter <= EndPtr
+            If TempStore(LeftCounter) <= TempStore(RightCounter) Then
+                Numbers(Counter) = TempStore(LeftCounter)
+                LeftCounter = LeftCounter + 1
+            Else
+                Numbers(Counter) = TempStore(RightCounter)
+                RightCounter = RightCounter + 1
+            End If
+            Counter += 1
+        End While
+        If LeftCounter <= MidPtr Then
+            For CopyCounter = 1 To MidPtr - LeftCounter + 1
+                Numbers(Counter + CopyCounter - 1) = TempStore(LeftCounter + CopyCounter - 1)
+            Next
+        Else
+            For CopyCounter = 1 To EndPtr - RightCounter + 1
+                Numbers(Counter + CopyCounter - 1) = TempStore(RightCounter + CopyCounter - 1)
+            Next
+        End If
+    End Sub
+
     Sub PartitionSort(Numbers() As Integer, Limit As Integer)
         'write Partition Sort code here
     End Sub
